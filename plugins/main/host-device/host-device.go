1d958e78087b518bb3a0e165f50c08313d52821c
Jimo Jung <xtmono@gmail.com>
2018-07-18 15:10:30 +0900

    Add ipam for host-device plugin
    - add ipam
    - add log

--- plugins/main/host-device/host-device.go
+++ plugins/main/host-device/host-device.go
@@ -17,6 +17,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net"
@@ -28,15 +29,18 @@ import (
 	"github.com/containernetworking/cni/pkg/types"
 	"github.com/containernetworking/cni/pkg/types/current"
 	"github.com/containernetworking/cni/pkg/version"
+	"github.com/containernetworking/plugins/pkg/ipam"
 	"github.com/containernetworking/plugins/pkg/ns"
 	"github.com/vishvananda/netlink"
+
+	"github.com/coreos/go-systemd/journal"
 )
 
 type NetConf struct {
 	types.NetConf
-	Device     string `json:"device"`     // Device-Name, something like eth0 or can0 etc.
-	HWAddr     string `json:"hwaddr"`     // MAC Address of target network interface
-	KernelPath string `json:"kernelpath"` // Kernelpath of the device
+	Devices     []string `json:"devices"`     // Device-Name, something like eth0 or can0 etc.
+	HWAddrs     []string `json:"hwaddrs"`     // MAC Address of target network interface
+	KernelPaths []string `json:"kernelpaths"` // Kernelpath of the device
 }
 
 func init() {
@@ -51,24 +55,47 @@ func loadConf(bytes []byte) (*NetConf, error) {
 	if err := json.Unmarshal(bytes, n); err != nil {
 		return nil, fmt.Errorf("failed to load netconf: %v", err)
 	}
-	if n.Device == "" && n.HWAddr == "" && n.KernelPath == "" {
+	if (n.Devices == nil || len(n.Devices) == 0) &&
+		(n.HWAddrs == nil || len(n.HWAddrs) == 0) &&
+		(n.KernelPaths == nil || len(n.KernelPaths) == 0) {
 		return nil, fmt.Errorf(`specify either "device", "hwaddr" or "kernelpath"`)
 	}
 	return n, nil
 }
 
+func logParam(args *skel.CmdArgs) {
+	journal.Print(journal.PriInfo, "  ContainerID: %s", args.ContainerID)
+	journal.Print(journal.PriInfo, "  Netns: %s", args.Netns)
+	journal.Print(journal.PriInfo, "  IfName: %s", args.IfName)
+	journal.Print(journal.PriInfo, "  Args: %s", args.Args)
+	journal.Print(journal.PriInfo, "  Path: %s", args.Path)
+	journal.Print(journal.PriInfo, "  StdinData: %s", string(args.StdinData))
+}
+
+func ResultString(r *types.Result) string {
+	data, err := json.Marshal(r)
+	if err != nil {
+		return ""
+	}
+	return string(data)
+}
+
 func cmdAdd(args *skel.CmdArgs) error {
+	journal.Print(journal.PriInfo, "Host-device CNI Add")
+	logParam(args)
+
 	cfg, err := loadConf(args.StdinData)
 	if err != nil {
 		return err
 	}
+
 	containerNs, err := ns.GetNS(args.Netns)
 	if err != nil {
 		return fmt.Errorf("failed to open netns %q: %v", args.Netns, err)
 	}
 	defer containerNs.Close()
 
-	hostDev, err := getLink(cfg.Device, cfg.HWAddr, cfg.KernelPath)
+	hostDev, err := getLink(cfg.Devices, cfg.HWAddrs, cfg.KernelPaths)
 	if err != nil {
 		return fmt.Errorf("failed to find host device: %v", err)
 	}
@@ -77,14 +104,84 @@ func cmdAdd(args *skel.CmdArgs) error {
 	if err != nil {
 		return fmt.Errorf("failed to move link %v", err)
 	}
-	return printLink(contDev, cfg.CNIVersion, containerNs)
-}
 
-func cmdDel(args *skel.CmdArgs) error {
-	_, err := loadConf(args.StdinData)
+	// Move out link if err to avoid link hold in this ns
+	defer func() {
+		if err != nil {
+			containerNs.Do(func(_ ns.NetNS) error {
+				return moveLinkOut(containerNs, args.IfName)
+			})
+		}
+	}()
+
+	// run the IPAM plugin and get back the config to apply
+	r, err := ipam.ExecAdd(cfg.IPAM.Type, args.StdinData)
 	if err != nil {
 		return err
 	}
+
+	// Invoke ipam del if err to avoid ip leak
+	defer func() {
+		if err != nil {
+			ipam.ExecDel(cfg.IPAM.Type, args.StdinData)
+		}
+	}()
+
+	// Convert whatever the IPAM result was into the current Result type
+	result, err := current.NewResultFromResult(r)
+	if err != nil {
+		return err
+	}
+	if len(result.IPs) == 0 {
+		return errors.New("IPAM plugin returned missing IP config")
+	}
+
+	result.Interfaces = []*current.Interface{
+		&current.Interface{
+			Name:    args.IfName,
+			Mac:     contDev.Attrs().HardwareAddr.String(),
+			Sandbox: containerNs.Path()}}
+
+	for _, ipc := range result.IPs {
+		// All addresses apply to the container interface
+		ipc.Interface = current.Int(0)
+	}
+
+	err = containerNs.Do(func(_ ns.NetNS) error {
+		if err := ipam.ConfigureIface(args.IfName, result); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	result.DNS = cfg.DNS
+
+	newResult, err := result.GetAsVersion(cfg.CNIVersion)
+	if err != nil {
+		return err
+	}
+	journal.Print(journal.PriInfo, "CNI Response: %s", ResultString(&newResult))
+	return newResult.Print()
+	//return types.PrintResult(result, cfg.CNIVersion)
+}
+
+func cmdDel(args *skel.CmdArgs) error {
+	journal.Print(journal.PriInfo, "Host-device CNI Del")
+	logParam(args)
+
+	cfg, err := loadConf(args.StdinData)
+	if err != nil {
+		return err
+	}
+
+	err = ipam.ExecDel(cfg.IPAM.Type, args.StdinData)
+	if err != nil {
+		return err
+	}
+
 	containerNs, err := ns.GetNS(args.Netns)
 	if err != nil {
 		return fmt.Errorf("failed to open netns %q: %v", args.Netns, err)
@@ -99,6 +196,9 @@ func cmdDel(args *skel.CmdArgs) error {
 }
 
 func moveLinkIn(hostDev netlink.Link, containerNs ns.NetNS, ifName string) (netlink.Link, error) {
+	if err := netlink.LinkSetDown(hostDev); err != nil {
+		return nil, fmt.Errorf("failed to host interface down %q: %v", ifName, err)
+	}
 	if err := netlink.LinkSetNsFd(hostDev, int(containerNs.Fd())); err != nil {
 		return nil, err
 	}
@@ -143,7 +243,11 @@ func moveLinkOut(containerNs ns.NetNS, ifName string) error {
 		if err != nil {
 			return fmt.Errorf("failed to find %q: %v", ifName, err)
 		}
+
 		// Rename device to it's original name
+		if err := netlink.LinkSetDown(dev); err != nil {
+			return fmt.Errorf("failed to container interface down %q: %v", ifName, err)
+		}
 		if err := netlink.LinkSetName(dev, dev.Attrs().Alias); err != nil {
 			return fmt.Errorf("failed to restore %q to original name %q: %v", ifName, dev.Attrs().Alias, err)
 		}
@@ -154,55 +258,50 @@ func moveLinkOut(containerNs ns.NetNS, ifName string) error {
 	})
 }
 
-func printLink(dev netlink.Link, cniVersion string, containerNs ns.NetNS) error {
-	result := current.Result{
-		CNIVersion: current.ImplementedSpecVersion,
-		Interfaces: []*current.Interface{
-			{
-				Name:    dev.Attrs().Name,
-				Mac:     dev.Attrs().HardwareAddr.String(),
-				Sandbox: containerNs.Path(),
-			},
-		},
-	}
-	return types.PrintResult(&result, cniVersion)
-}
-
-func getLink(devname, hwaddr, kernelpath string) (netlink.Link, error) {
+func getLink(devices, hwaddrs, kernelpaths []string) (netlink.Link, error) {
 	links, err := netlink.LinkList()
 	if err != nil {
 		return nil, fmt.Errorf("failed to list node links: %v", err)
 	}
 
-	if len(devname) > 0 {
-		return netlink.LinkByName(devname)
-	} else if len(hwaddr) > 0 {
-		hwAddr, err := net.ParseMAC(hwaddr)
-		if err != nil {
-			return nil, fmt.Errorf("failed to parse MAC address %q: %v", hwaddr, err)
+	for _, device := range devices {
+		if link, err := netlink.LinkByName(device); err == nil {
+			return link, nil
 		}
+	}
 
-		for _, link := range links {
-			if bytes.Equal(link.Attrs().HardwareAddr, hwAddr) {
-				return link, nil
+	for _, hwaddr := range hwaddrs {
+		if hwAddr, err := net.ParseMAC(hwaddr); err != nil {
+			journal.Print(journal.PriErr,
+				"failed to parse MAC address %q: %v", hwaddr, err)
+		} else {
+			for _, link := range links {
+				if bytes.Equal(link.Attrs().HardwareAddr, hwAddr) {
+					return link, nil
+				}
 			}
 		}
-	} else if len(kernelpath) > 0 {
-		if !filepath.IsAbs(kernelpath) || !strings.HasPrefix(kernelpath, "/sys/devices/") {
-			return nil, fmt.Errorf("kernel device path %q must be absolute and begin with /sys/devices/", kernelpath)
-		}
-		netDir := filepath.Join(kernelpath, "net")
-		files, err := ioutil.ReadDir(netDir)
-		if err != nil {
-			return nil, fmt.Errorf("failed to find network devices at %q", netDir)
-		}
+	}
 
-		// Grab the first device from eg /sys/devices/pci0000:00/0000:00:19.0/net
-		for _, file := range files {
-			// Make sure it's really an interface
-			for _, l := range links {
-				if file.Name() == l.Attrs().Name {
-					return l, nil
+	for _, kernelpath := range kernelpaths {
+		if !filepath.IsAbs(kernelpath) || !strings.HasPrefix(kernelpath, "/sys/devices/") {
+			journal.Print(journal.PriErr,
+				"kernel device path %q must be absolute and begin with /sys/devices/", kernelpath)
+		} else {
+			netDir := filepath.Join(kernelpath, "net")
+			files, err := ioutil.ReadDir(netDir)
+			if err != nil {
+				journal.Print(journal.PriErr,
+					"failed to find network devices at %q", netDir)
+			} else {
+				// Grab the first device from eg /sys/devices/pci0000:00/0000:00:19.0/net
+				for _, file := range files {
+					// Make sure it's really an interface
+					for _, l := range links {
+						if file.Name() == l.Attrs().Name {
+							return l, nil
+						}
+					}
 				}
 			}
 		}
